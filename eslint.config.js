d90a3381477d4ba374b869489402ecd31b1df992
xzc <xuzhichao1618@qq.com>
2025-02-05 15:30:53 +0800

    feat: 添加mdx语法检查

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,41 @@
 import eslint from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
-import reactPlugin from "eslint-plugin-react";
+import tsParser from "@typescript-eslint/parser";
 import importPlugin from "eslint-plugin-import";
+import * as mdx from "eslint-plugin-mdx";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   eslint.configs.recommended,
+  // mdx配置
+  {
+    ...mdx.flat,
+    rules: {
+      ...mdx.flat.rules,
+      "no-var": "off",
+      parsing: "off",
+      "prefer-const": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+    // optional, if you want to lint code blocks at the same
+    processor: mdx.createRemarkProcessor({
+      lintCodeBlocks: true,
+      // optional, if you want to disable language mapper, set it to `false`
+      // if you want to override the default language mapper inside, you can provide your own
+      languageMapper: {},
+    }),
+  },
+  // mdx代码块配置
+  {
+    ...mdx.flatCodeBlocks,
+    rules: {
+      ...mdx.flatCodeBlocks.rules,
+      // if you want to override some rules for code blocks
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
   {
     files: ["**/*.{js,jsx,ts,tsx,mjs,cjs}"],
     languageOptions: {
